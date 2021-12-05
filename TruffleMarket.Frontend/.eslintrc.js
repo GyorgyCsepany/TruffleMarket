69d05a3076c89842ecbb4d585ca306656b8e10f3
GyorgyCsepany <gyorgy.csepany@realeyesit.com>
2021-12-05 23:27:03 +0100

    Add defineProps as global to eslint

--- TruffleMarket.Frontend/.eslintrc.js
+++ TruffleMarket.Frontend/.eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   env: {
     node: true,
   },
+  globals: {
+    defineProps: "readonly",
+  },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     // override/add rules settings here, such as:
