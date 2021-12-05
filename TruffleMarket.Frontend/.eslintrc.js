1a0fcfeaf85459c225a497bf213634b0877fb75d
GyorgyCsepany <gyorgy.csepany@realeyesit.com>
2021-12-05 23:29:10 +0100

    Turn  off vue/require-default-prop

--- TruffleMarket.Frontend/.eslintrc.js
+++ TruffleMarket.Frontend/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "vue/require-default-prop": "off",
   },
 };
