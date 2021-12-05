0d2d1df4e24f43c214a7dd4fa912bd3b70ce847b
GyorgyCsepany <gyorgy.csepany@realeyesit.com>
2021-12-05 23:31:19 +0100

    Set parserOptions to eslint

--- TruffleMarket.Frontend/.eslintrc.js
+++ TruffleMarket.Frontend/.eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
   globals: {
     defineProps: "readonly",
   },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     "vue/require-default-prop": "off",
