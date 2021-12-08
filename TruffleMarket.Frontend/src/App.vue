5a2de2232fe7f3d9b97dbbb5eee3ac34b0c4994e
GyorgyCsepany <gyorgy.csepany@realeyesit.com>
2021-12-08 23:59:08 +0100

    Add the header

--- TruffleMarket.Frontend/src/App.vue
+++ TruffleMarket.Frontend/src/App.vue
@@ -1,21 +1,64 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+
+let activeTab = ref("firstTab");
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <el-container>
+    <el-header>
+      <el-tabs v-model="activeTab" @tab-click="handleClick">
+        <el-tab-pane label="Active items" name="firstTab"></el-tab-pane>
+        <el-tab-pane label="Your bids" name="secondTab"></el-tab-pane>
+        <el-tab-pane label="Your offers" name="thirdTab"></el-tab-pane>
+        <el-tab-pane label="Submit an offer" name="fourthTab"></el-tab-pane>
+      </el-tabs>
+    </el-header>
+    <el-main><HelloWorld /></el-main>
+  </el-container>
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0px;
+}
+
+.el-header {
+  text-transform: uppercase;
+  background-color: #194461;
+  line-height: 60px;
+  padding-left: max(15px, 10%);
+  padding-right: max(15px, 10%);
+}
+
+.el-main {
+  padding: 20px max(15px, 10%);
+}
+
+.el-tabs__header .el-icon svg {
+  height: 60px;
+}
+
+.el-tabs__item {
+  color: rgb(199, 198, 198);
+  font-weight: 600;
+}
+
+.el-tabs__item.is-active,
+.el-tabs__item:hover {
+  color: white;
+}
+
+.el-tabs__active-bar {
+  background-color: white;
+}
+
+.el-tabs__nav-wrap::after {
+  background-color: transparent;
 }
 </style>
